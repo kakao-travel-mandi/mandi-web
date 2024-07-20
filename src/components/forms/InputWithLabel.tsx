d4859d5b465581859a0bf9a18937236f2c5c844f
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-07-20 09:51:32 +0900

    Feat: 프로필 변경 페이지 검증 로직 추가

--- src/components/forms/InputWithLabel.tsx
+++ src/components/forms/InputWithLabel.tsx
@@ -1,18 +1,28 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { forwardRef } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+import { clsx } from "clsx";
 
 interface InputWithLabelProps {
   title: string;
   value: string;
   required?: boolean;
+  register?: UseFormRegisterReturn;
+  isFormInvalid?: boolean;
 }
 
 export const InputWithLabel = forwardRef<HTMLInputElement, InputWithLabelProps>(
-  ({ title, required, value }, ref) => {
+  ({ title, required, value, register, isFormInvalid }, ref) => {
     return (
-      <div className="grid w-full max-w-xl items-center gap-1.5">
-        <Label htmlFor={value} className="text-lg">
+      <div className="flex flex-col w-full max-w-xl items-start gap-1.5">
+        <Label
+          htmlFor={value}
+          className={clsx(
+            "text-[12px] text-gray-700",
+            isFormInvalid && "text-red-600"
+          )}
+        >
           {title}
           <span className="text-red-600 leading-6">
             {required ? "*" : null}
@@ -23,7 +33,11 @@ export const InputWithLabel = forwardRef<HTMLInputElement, InputWithLabelProps>(
           id={value}
           ref={ref}
           placeholder={title}
-          className=" bg-gray-200 border-gray-300 p-5 placeholder:text-gray-700 text-lg"
+          {...register}
+          className={clsx(
+            "bg-gray-200 border-gray-300 p-5 placeholder:text-gray-500 text-[16px]",
+            isFormInvalid && "bg-red-light border-none"
+          )}
         />
       </div>
     );
--- src/pages/ProfileSetting.tsx
+++ src/pages/ProfileSetting.tsx
@@ -2,10 +2,27 @@ import { dummyUser as user } from "@/pages/MyPage";
 import defaultUserImg from "/assets/pngs/default.png";
 import { CameraIcon } from "lucide-react";
 import { InputWithLabel } from "@/components/forms/InputWithLabel";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button.tsx";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 export default function ProfileSettingPage() {
+  const { register, handleSubmit, formState } = useForm<{
+    nickName: string;
+    introduce: string;
+  }>();
+
+  useEffect(() => {
+    if (formState.errors.nickName?.type === "maxLength") {
+      toast("최대 10자까지 입력 가능해요.", {
+        type: "error",
+        closeButton: false,
+      });
+    }
+  }, [formState.errors.nickName]);
   return (
-    <div className="h-full justify-between flex flex-col items-center px-4 gap-4 py-10 bg-white">
+    <div className="h-full flex flex-col items-center px-4 py-10 bg-white">
       <div className="w-full flex flex-col justify-center items-center gap-7">
         <div className="max-w-fit relative flex items-center gap-x-4">
           <img
@@ -17,12 +34,54 @@ export default function ProfileSettingPage() {
             <CameraIcon size={20} fill="white" />
           </div>
         </div>
-        <InputWithLabel title="이름" value="name" required />
-        <InputWithLabel title="한 줄 소개" value="introduce" />
+        <form
+          className="w-full flex flex-col gap-[28px]"
+          onSubmit={handleSubmit((data) => console.log(data))}
+        >
+          <div className="flex flex-col gap-[8px]">
+            <InputWithLabel
+              title="닉네임"
+              value="nickName"
+              required
+              register={register("nickName", {
+                required: "닉네임을 입력해주세요.",
+                minLength: {
+                  value: 2,
+                  message: "닉네임은 2자 이상이어야 합니다.",
+                },
+                maxLength: {
+                  value: 10,
+                  message: "닉네임은 10자 이하여야 합니다.",
+                },
+              })}
+              isFormInvalid={Boolean(formState.errors.nickName)}
+            />
+            <span className="text-[12px] text-red-400">
+              {formState.errors.nickName?.message}
+            </span>
+          </div>
+          <div className="flex flex-col gap-[8px]">
+            <InputWithLabel
+              title="한 줄 소개"
+              value="introduce"
+              register={register("introduce", {
+                maxLength: {
+                  value: 40,
+                  message: "한 줄 소개는 40자 이하여야 합니다.",
+                },
+              })}
+              isFormInvalid={Boolean(formState.errors.introduce)}
+            />
+            <span>{formState.errors.introduce?.message}</span>
+          </div>
+          <Button
+            className="flex justify-center items-center w-[90%] h-[52px] text-white rounded-lg bg-green-500 hover:bg-green-400 hover:text-white"
+            variant="ghost"
+          >
+            저장
+          </Button>
+        </form>
       </div>
-      <button className="bg-green-500 w-[90%] p-5 text-white rounded-lg">
-        저장
-      </button>
     </div>
   );
 }
