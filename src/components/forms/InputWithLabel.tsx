5db2e319ee253d330324348da9c45c879d66a6d2
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-07-20 09:05:45 +0900

    Refactor: InputWithLabel forwardRef로 변경

--- src/components/forms/InputWithLabel.tsx
+++ src/components/forms/InputWithLabel.tsx
@@ -1,27 +1,31 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { forwardRef } from "react";
 
-export function InputWithLabel({
-  title,
-  value,
-  required,
-}: {
+interface InputWithLabelProps {
   title: string;
   value: string;
   required?: boolean;
-}) {
-  return (
-    <div className="grid w-full max-w-xl items-center gap-1.5">
-      <Label htmlFor={value} className="text-lg">
-        {title}
-        <span className="text-red-600 leading-6">{required ? "*" : null}</span>
-      </Label>
-      <Input
-        type={value}
-        id={value}
-        placeholder={title}
-        className=" bg-gray-200 border-gray-300 p-5 placeholder:text-gray-700 text-lg"
-      />
-    </div>
-  );
 }
+
+export const InputWithLabel = forwardRef<HTMLInputElement, InputWithLabelProps>(
+  ({ title, required, value }, ref) => {
+    return (
+      <div className="grid w-full max-w-xl items-center gap-1.5">
+        <Label htmlFor={value} className="text-lg">
+          {title}
+          <span className="text-red-600 leading-6">
+            {required ? "*" : null}
+          </span>
+        </Label>
+        <Input
+          type={value}
+          id={value}
+          ref={ref}
+          placeholder={title}
+          className=" bg-gray-200 border-gray-300 p-5 placeholder:text-gray-700 text-lg"
+        />
+      </div>
+    );
+  }
+);
