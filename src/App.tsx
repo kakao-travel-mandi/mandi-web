b85448e61499cfb74443516e6a65baa9d0ce7156
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-07-22 00:05:38 +0900

    Chore: 페이지 외부 영역 배경색 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from "react-router-dom";
 
 function App() {
   return (
-    <div className=" w-[100vw] h-[100dvh] flex flex-col justify-between overflow-hidden">
+    <div className="bg-gray-100 w-[100vw] h-[100dvh] flex flex-col justify-between overflow-hidden">
       <RouterProvider router={Router} />
     </div>
   );
