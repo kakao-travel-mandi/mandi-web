b4b4b31beef079ff6a7308312fddf56199e23b0b
조원주 <dnjswn9178@gmail.com>
2024-07-27 15:02:29 +0900

    feat: profile leave fin

--- src/pages/ProfileLeave.tsx
+++ src/pages/ProfileLeave.tsx
@@ -1,8 +1,24 @@
 import trashcanImg from "/assets/pngs/trashcan.png";
 import discImg from "/assets/pngs/disc.png";
 import { Button } from "@/components/ui/button";
+import { CheckCircle } from "@/components/icons";
+import { useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useNavigate } from "react-router-dom";
+import { ROUTE_PATH } from "@/routers/routePath";
 
 function ProfileLeave() {
+  const [isAgree, setIsAgree] = useState<boolean>(false);
+  const navigation = useNavigate();
+
   const leaveCondition = [
     {
       id: 1,
@@ -39,14 +55,68 @@ function ProfileLeave() {
           </div>
         ))}
       </div>
-      <div className="relative bottom-0">
+      <div className="absolute bottom-2">
+        <div className="flex items-center justify-center space-x-2 mb-4">
+          <Button
+            variant="ghost"
+            className="flex justify-start space-x-2 hover:bg-transparent"
+            onClick={() => setIsAgree((pre) => !pre)}
+          >
+            <CheckCircle
+              width={20}
+              height={20}
+              color={isAgree ? "#00B288" : ""}
+            />
+            <span className="text-sm text-gray-800">
+              위 주의사항을 모두 숙지했고, 탈퇴에 동의합니다
+            </span>
+          </Button>
+        </div>
         <div className="flex w-full space-x-3">
-          <Button className="w-full py-3.5 bg-gray-100 text-gray-600">
+          <Button
+            variant="secondary"
+            className="w-full py-3.5 bg-gray-100 text-gray-600"
+          >
             더 써볼래요
           </Button>
-          <Button className="w-full py-3.5 bg-green-100 text-white">
-            탈퇴하기
-          </Button>
+          <div className="w-full">
+            <AlertDialog>
+              <AlertDialogTrigger
+                disabled={isAgree ? false : true}
+                className="w-full"
+              >
+                <Button
+                  className={`${
+                    isAgree ? "bg-green" : "bg-green-100 "
+                  } w-full py-3.5 text-white hover:bg-green-600`}
+                  disabled={isAgree ? false : true}
+                >
+                  탈퇴하기
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent className="bg-white border gap-0 space-y-6 border-none pt-7 pb-4 px-4 max-w-80">
+                <div className="space-y-1">
+                  <AlertDialogTitle className="text-lg text-gray-800">
+                    회원탈퇴
+                  </AlertDialogTitle>
+                  <AlertDialogDescription className="text-sm text-gray-600">
+                    정말 회원탈퇴할까요?
+                  </AlertDialogDescription>
+                </div>
+                <div className="flex w-full space-x-3">
+                  <AlertDialogCancel className="w-full bg-gray-100 border border-none text-gray-600">
+                    취소
+                  </AlertDialogCancel>
+                  <AlertDialogAction
+                    className="w-full bg-red text-white"
+                    onClick={() => navigation(ROUTE_PATH.LOGIN.path)}
+                  >
+                    탈퇴하기
+                  </AlertDialogAction>
+                </div>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
         </div>
       </div>
     </div>
